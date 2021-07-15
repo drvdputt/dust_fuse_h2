6e3373db6a00b5aa6cf562571e883b42eb30cd87
Dries Van De Putte <dvandeputte@stsci.edu>
2021-07-15 10:42:47 -0400

    Look for HST (STIS) data for all 23 stars that need HI

--- download_HST_data.py
+++ download_HST_data.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 
 def main():
-    with open("HST_star_list.txt", "r") as f:
+    with open("list_for_data_lookup.csv", "r") as f:
         star_names = [s.strip(" \n") for s in f.readlines()]
 
+    data_found = {}
     for target in star_names:
         # results = mast.Observations.query_object(
         #     name, radius="0.1 arcmin", dataproduct_type=["spectrum"]
@@ -15,11 +16,16 @@ def main():
             radius="0.1 arcmin",
             dataproduct_type=["spectrum"],
         )
-        process_mast_results(target, results)
+        count = process_mast_results(target, results)
+        data_found[target] = count
 
+    print("Data search/download overview (number of relevant STIS spectra found)")
+    for key, value in data_found.items():
+        print(key, ' ', value)
 
 def process_mast_results(target, results):
     print(target)
+    count = 0
     for row in results:
         i = row["instrument_name"]
         f = row["filters"]
@@ -37,6 +43,8 @@ def process_mast_results(target, results):
             manifest = mast.Observations.download_products(
                 products, productType="SCIENCE", download_dir=str(target_dir)
             )
+            count += 1
+    return count
 
 
 main()

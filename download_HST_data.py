ad0934ca82aa56eb3b58146715775fee0a7ef2aa
Dries Van De Putte <dvandeputte@stsci.edu>
2021-07-19 10:45:17 -0400

    Write out better overview of data available

--- download_HST_data.py
+++ download_HST_data.py
@@ -19,31 +19,36 @@ def main():
         count = process_mast_results(target, results)
         data_found[target] = count
 
-    print("Data search/download overview (number of relevant STIS spectra found)")
+    print("Data search/download overview (number of spectra found)")
     for key, value in data_found.items():
-        print(key, ' ', value)
+        print(key, " ", value)
+
 
 def process_mast_results(target, results):
     print(target)
-    count = 0
+    count = {}
     for row in results:
+        o = row["obs_collection"]
         i = row["instrument_name"]
         f = row["filters"]
-        if "STIS" in i and f == "E140H":
+        print("\t", o, i, f)
+
+        if "STIS" in i and (f == "E140H" or f == "E140M"):
             target_dir = Path.cwd() / "data" / target
             target_dir.mkdir(exist_ok=True)
-
-            # for c, cc in zip(results.colnames, results[1]):
-            #     print(c, cc)
-
-            # obsid = row['obs_id']
             products = mast.Observations.get_product_list(row)
-            print("Found the following products for {}:".format(target))
-            print(products)
             manifest = mast.Observations.download_products(
                 products, productType="SCIENCE", download_dir=str(target_dir)
             )
-            count += 1
+            count["STIS"] = count.get("STIS", 0) + 1
+
+        # count these but don't download them (I already have them)
+        if "SWP" in i:
+            if "HIGH" in f:
+                count["IUE H"] = count.get("IUE H", 0) + 1
+            if "LOW" in f:
+                count["IUE L"] = count.get("IUE L", 0) + 1
+
     return count
 
 

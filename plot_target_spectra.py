be3524fa221761906a768ced0b91015088badd76
Dries Van De Putte <dvandeputte@stsci.edu>
2021-08-04 17:36:21 -0400

    Script to plot overview of spectral data for specific target
    Can be used to check if the spectra align properly

---
+++ plot_target_spectra.py
@@ -0,0 +1,37 @@
+"""Investigate different spectra by the same instrument for a certain target."""
+import get_spectrum
+import argparse
+from pathlib import Path
+from matplotlib import pyplot as plt
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target")
+    parser.add_argument("data_type", choices=["STIS", "IUEH", "IUEL"])
+    args = parser.parse_args()
+    target = args.target
+    data_type = args.data_type
+
+    data_dir = Path("./data") / target
+    if data_type == "STIS":
+        filenames = data_dir.glob("**/*_x1d.fits")
+    elif data_type == "IUEH":
+        filenames = data_dir.glob("*mxhi.gz")
+    elif data_type == "IUEL":
+        filenames = data_dir.glob("*mxlo_vo.fits")
+
+    fig, ax = plt.subplots()
+    for fn in filenames:
+        plot_spectrum(ax, fn)
+
+    plt.show()
+
+
+def plot_spectrum(ax, filename):
+    wavs, flux, errs, _ = get_spectrum.auto_wavs_flux_errs(str(filename))
+    ax.plot(wavs, flux, alpha=0.5)
+
+
+if __name__ == "__main__":
+    main()

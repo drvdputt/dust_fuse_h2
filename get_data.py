819175177decb8e28ffb1ac42d74ecbb3fb712d6
Dries Van De Putte <dvandeputte@stsci.edu>
2021-12-23 12:13:53 -0500

    Move function to evaluate FM90 function to class

--- get_data.py
+++ get_data.py
@@ -552,58 +552,14 @@ def get_merged_table(comp=False):
     # only try this if all FM90 parameters are present
     params_needed = [f"CAV{n}" for n in range(1, 5)] + ["gamma", "x_o"]
     if all([p in merged_table.colnames for p in params_needed]):
-        # get FM90 params and their uncertainties
-        cav1, cav2, cav3, cav4, gamma, x0 = [merged_table[p] for p in params_needed]
-        cav1_unc, cav2_unc, cav3_unc, cav4_unc, gamma_unc, x0_unc = [
-            merged_table[p + "_unc"] for p in params_needed
-        ]
 
-        micron = 10000
-
-        def extinction_curve(w):
-            """
-            Evaluate the FM90 function for all sightlines, at a given wavelength.
-
-            w : wavelength in angstrom
-            """
-            # x is 1/w in micron
-            x = 1 / (w / micron)
-            # drude
-            D = x ** 2 / ((x ** 2 - x0 ** 2) ** 2 + (x * gamma) ** 2)
-            F = 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
-            Aw_AV = cav1 + cav2 * x + cav3 * D + cav4 * F
-            return Aw_AV
-
-        def extinction_curve_unc(w):
-            """
-            Uncertainty on the FM90 function, as propagated from the parameter uncertainties.
-            """
-            x = 1 / (w / micron)
-
-            # drude
-            D_denom = (x ** 2 - x0 ** 2) ** 2 + (x * gamma) ** 2
-            D = x ** 2 / D_denom
-            F = 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
-
-            # dD / dg = -2gx^4 / (g^2 x^2 + (x0^2 + x^2)^2)^2
-            # dD / dx0 = -4mx^2(m^2+x^2)/(g^2x^2+(m^2+x^2)^2)^2
-
-            # multivariate error propagation
-            D_unc_gamma = 2 * gamma * x ** 4 / D_denom ** 2 * gamma_unc
-            D_unc_x0 = 4 * x0 * x ** 2 * (x ** 2 - x0 ** 2) / D_denom ** 2 * x0_unc
-            VD_rel = (D_unc_gamma ** 2 + D_unc_x0 ** 2) / D ** 2
-            # sum of relative variances
-            Vcav3_rel = (cav3_unc / cav3) ** 2
-            Vterm3 = (cav3 * D) ** 2 * (Vcav3_rel + VD_rel)
-
-            VA = cav1_unc ** 2 + (cav2_unc * x) ** 2 + Vterm3 + (cav4_unc * F) ** 2
-            return np.sqrt(VA)
+        ec = ExtinctionCurveSet(merged_table)
 
         def add_specific_wavelength(w):
             val = f"A{w}_AV"
             unc = val + "_unc"
-            merged_table[val] = extinction_curve(w)
-            merged_table[unc] = extinction_curve_unc(w)
+            merged_table[val] = ec.evaluate(w)
+            merged_table[unc] = ec.evaluate_unc(w)
             rel_unc = merged_table[unc] / merged_table[val]
 
             # also multiply them by AV, but do not add AV error, since
@@ -619,17 +575,90 @@ def get_merged_table(comp=False):
             nhtot_Alambda_rel_unc = np.sqrt(
                 rel_unc ** 2 + (merged_table["nhtot_unc"] / merged_table["nhtot"]) ** 2
             )
-            merged_table["NH_" + absval + "_unc"] = nhtot_Alambda * nhtot_Alambda_rel_unc
+            merged_table["NH_" + absval + "_unc"] = (
+                nhtot_Alambda * nhtot_Alambda_rel_unc
+            )
 
             # also add inverse of this for easier interpretation
             merged_table[absval + "_NH"] = 1 / nhtot_Alambda
-            merged_table[absval + "_NH_unc"] = merged_table[absval + "_NH"] * nhtot_Alambda_rel_unc
+            merged_table[absval + "_NH_unc"] = (
+                merged_table[absval + "_NH"] * nhtot_Alambda_rel_unc
+            )
 
         add_specific_wavelength(1000)
         # add_den_column("A1000", "A1000_d")
         add_specific_wavelength(2175)
 
     return merged_table
+
+
+class ExtinctionCurveSet:
+    """Tools to evaluate FM90 function for many stars at once"""
+
+    micron = 10000
+    params_needed = [f"CAV{n}" for n in range(1, 5)] + ["gamma", "x_o"]
+
+    def __init__(self, data):
+        """data : astropy table containing the named columns "CAV[1-4]", "x_o", "gamma" """
+        # unpack like this, so the equations are more readable
+        (self.cav1, self.cav2, self.cav3, self.cav4, self.gamma, self.x0) = [
+            data[p] for p in ExtinctionCurveSet.params_needed
+        ]
+        (
+            self.cav1_unc,
+            self.cav2_unc,
+            self.cav3_unc,
+            self.cav4_unc,
+            self.gamma_unc,
+            self.x0_unc,
+        ) = [data[p + "_unc"] for p in ExtinctionCurveSet.params_needed]
+
+    def evaluate(self, w):
+        """
+        Evaluate the FM90 function for all sightlines, at a given wavelength.
+
+        w : wavelength in angstrom
+        """
+        # x is 1/w in micron
+        x = 1 / (w / ExtinctionCurveSet.micron)
+        # drude
+        D = x ** 2 / ((x ** 2 - self.x0 ** 2) ** 2 + (x * self.gamma) ** 2)
+        F = 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
+        Aw_AV = self.cav1 + self.cav2 * x + self.cav3 * D + self.cav4 * F
+        return Aw_AV
+
+    def evaluate_unc(self, w):
+        """
+        Uncertainty on the FM90 function, as propagated from the parameter
+        uncertainties.
+        """
+        x = 1 / (w / ExtinctionCurveSet.micron)
+
+        # drude
+        D_denom = (x ** 2 - self.x0 ** 2) ** 2 + (x * self.gamma) ** 2
+        D = x ** 2 / D_denom
+        F = 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
+
+        # dD / dg = -2gx^4 / (g^2 x^2 + (x0^2 + x^2)^2)^2
+        # dD / dx0 = -4mx^2(m^2+x^2)/(g^2x^2+(m^2+x^2)^2)^2
+
+        # multivariate error propagation
+        D_unc_gamma = 2 * self.gamma * x ** 4 / D_denom ** 2 * self.gamma_unc
+        D_unc_x0 = (
+            4 * self.x0 * x ** 2 * (x ** 2 - self.x0 ** 2) / D_denom ** 2 * self.x0_unc
+        )
+        VD_rel = (D_unc_gamma ** 2 + D_unc_x0 ** 2) / D ** 2
+        # sum of relative variances
+        Vcav3_rel = (self.cav3_unc / self.cav3) ** 2
+        Vterm3 = (self.cav3 * D) ** 2 * (Vcav3_rel + VD_rel)
+
+        VA = (
+            self.cav1_unc ** 2
+            + (self.cav2_unc * x) ** 2
+            + Vterm3
+            + (self.cav4_unc * F) ** 2
+        )
+        return np.sqrt(VA)
 
 
 if __name__ == "__main__":

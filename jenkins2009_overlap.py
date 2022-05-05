14b8056d79ea9bd5ac9db2cde61353a131b2e5d0
Dries Van De Putte <dvandeputte@stsci.edu>
2022-05-05 19:39:22 -0400

    Draft for comparing NH / AV to F*

--- jenkins2009_overlap.py
+++ jenkins2009_overlap.py
@@ -1,3 +1,4 @@
+import numpy as np
 from astropy.table import Table
 tjenkins = Table.read('data/jenkins2009-table2.dat', format='ascii.cds')
 
@@ -16,3 +17,13 @@ hd_set = set(our_hd_numbers) & set(jenkins_hd_numbers[~jenkins_hd_numbers.mask])
 print(f"The following {len(hd_set)} HD stars overlap")
 print(hd_set)
 print(f"And also these two that I found manually: BD+35d4258 and BD+53d2820")
+
+# this does not work without converting to list first, for some reason
+tjenkins_overlap = np.isin(tjenkins['HD'], list(hd_set)) 
+# our data overlap based on number
+data_overlap = [name[:2] == 'HD' and int(name[2:]) in hd_set for name in data['Name']]
+
+nhav = data['NH_AV'][data_overlap]
+
+# to get fstar, we need to load another table...
+fstar = tjenkins_depletion['Fstar'][tjenkins_overlap]

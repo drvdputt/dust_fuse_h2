852aeedcd50109dd97fcfa69b2f6bdd125481168
Dries Van De Putte <dvandeputte@stsci.edu>
2022-05-05 17:44:36 -0400

    Reworked paper histograms plots

--- paper_histograms.py
+++ paper_histograms.py
@@ -3,10 +3,11 @@ import paper_rcparams
 import get_data
 import pearson
 from matplotlib import pyplot as plt
-from plot_fuse_results import format_colname
+from plot_fuse_results import plot_results_scatter
+from paper_scatter import data
+import numpy as np
 
-
-data = get_data.get_merged_table()
+# use same data as for the scatter plots
 
 
 def rho_histogram(ax, xparam, yparam, comment, vert_label=False):
@@ -16,73 +17,95 @@ def rho_histogram(ax, xparam, yparam, comment, vert_label=False):
     """
     annotation_size = 9
     xs, ys, covs = get_data.get_xs_ys_covs(data, xparam, yparam)
-    rho_median, std_null = pearson.pearson_mc_nocov(xs, ys, covs, hist_ax=ax)
-    xlabel = format_colname(xparam).split(" ")[0]
-    ylabel = format_colname(yparam).split(" ")[0]
-    text = f"$x = ${xlabel}\n$y = ${ylabel}\n{comment}"
-    ax.text(0.03, 0.9, text, transform=ax.transAxes, va="top", fontsize=annotation_size)
+    results = pearson.new_rho_method(xs, ys, covs, hist_ax=ax)
+    rho_measured, null_std, numsigma = (
+        results["measured_rho"],
+        results["null_std"],
+        results["numsigma"],
+    )
+    # xlabel = format_colname(xparam).split(" ")[0]
+    # ylabel = format_colname(yparam).split(" ")[0]
+    # text = f"$x = ${xlabel}\n$y = ${ylabel}\n{comment}"
+    # ax.text(0.03, 0.9, text, transform=ax.transAxes, va="top", fontsize=annotation_size)
     ax.tick_params("x", direction="inout")
 
     def weighted_y(f):
         return (1 - f) * ax.get_ylim()[0] + f * ax.get_ylim()[1]
 
-    x_offset = (ax.get_xlim()[1] - ax.get_xlim()[0]) * 0.01
+    x_offset = (ax.get_xlim()[1] - ax.get_xlim()[0]) * 0.06
     y_offset = (ax.get_ylim()[1] - ax.get_ylim()[0]) * 0.04
 
-    # indicate median rho
-    ax.axvline(rho_median, color="xkcd:bright blue")
-    if vert_label:
-        ax.text(rho_median + x_offset, weighted_y(0.8), "median", color="k", ha="left")
+    # indicate nullrho
+    null_rho = results["null_rho"]
+    ax.axvline(null_rho, color="xkcd:bright blue")
 
-    # indicate null sigma
-    ax.axvline(0, color="xkcd:gray")
-    # if vert_label:
-    #     ax.text(0 + x_offset, weighted_y(0.8), "$\\rho_0 = 0$", color="k", ha="left")
+    def vertical_line_label(x, text, color):
+        if vert_label:
+            ax.text(
+                x - x_offset,
+                weighted_y(0.6),
+                text,
+                color=color,
+                ha="left",
+                va="bottom",
+                rotation="vertical",
+            )
 
-    # 1 sigma arrow
-    # ax.arrow(0, weighted_y(0.5), std_null, 0)
-    height1 = weighted_y(0.6)
-    ax.annotate(
-        "",
-        xy=(std_null, height1),
-        xytext=(0, height1),
-        xycoords="data",
-        arrowprops=dict(arrowstyle="->"),
-    )
-    ax.text(
-        std_null * 0.8,
-        height1 + y_offset,
-        "$\\sigma(r_0)$",
-        ha="center",
-        fontsize=annotation_size,
-    )
-    # n sigmfa arrow
-    # ax.arrow(0, weighted_y(0.4), rho_median, 0, length_includes_head=True, head_length=3, head_width=3)
-    height2 = weighted_y(0.4)
-    ax.annotate(
-        "",
-        xy=(rho_median, height2),
-        xytext=(0, height2),
-        xycoords="data",
-        arrowprops=dict(arrowstyle="->"),
-    )
-    ax.text(
-        rho_median * 0.8,
-        height2 + y_offset,
-        f"${rho_median / std_null:.1f}\\sigma$",
-        ha="right",
-        fontsize=annotation_size,
-    )
+    # indicate measured rho
+    vertical_line_label(rho_measured, "measured", 'k')
+    # indicate rho intrinsic to null hypothesis
+    vertical_line_label(null_rho, "induced offset", "xkcd:bright blue")
+
+    # indicate zero vertical
+    ax.axvline(0, color="k", alpha=0.5, ls="dotted", lw=1)
 
 
-fig, ax = plt.subplots(2, 1, sharex=True)
+    def numsigma_arrow(ypos, start, end):
+        height = weighted_y(ypos)
+        ax.annotate(
+            "",
+            xy=(end, height),
+            xytext=(start, height),
+            xycoords="data",
+            arrowprops=dict(arrowstyle="->"),
+        )
+        ax.text(
+            end - x_offset,
+            height,
+            f"${np.abs(rho_measured - start) / null_std:.1f}\\sigma$",
+            ha="right",
+            va="center",
+            fontsize=annotation_size,
+        )
 
-rho_histogram(ax[0], "1_RV", "NH_AV", "not significant")
-rho_histogram(ax[1], "A1000_AV", "NH_AV", "significant", vert_label=True)
+    numsigma_arrow(0.4, null_rho, rho_measured)
+    numsigma_arrow(0.2, 0, rho_measured)
 
-ax[0].legend()
-ax[1].set_xlabel("$r$")
-ax[1].set_ylabel("number of $r$ samples")
-fig.set_size_inches(paper_rcparams.column_width, paper_rcparams.column_width * 1)
-fig.subplots_adjust(hspace=0, top=0.996, right=0.996)
+
+fig, axs = plt.subplots(2, 1)
+
+# rho_histogram(axs[0], "1_RV", "NH_AV", "not significant")
+
+# plot fh2 vs nh_av as demonstration
+max_nh_av = 5e21
+xs, ys, covs = plot_results_scatter(
+    axs[0],
+    data,
+    "fh2",
+    "NH_AV",
+    pyrange=[0, max_nh_av],
+    # data_comp=comp,
+    ignore_comments=["hi_h_av"],
+    # mark_comments=MARK_STRING,
+)
+
+rho_histogram(axs[1], "fh2", "NH_AV", "", vert_label=True)
+
+axs[1].legend(loc="upper right")
+axs[1].set_xlabel("$r$")
+axs[1].set_ylabel("number of $r$ samples")
+fig.set_size_inches(paper_rcparams.column_width, paper_rcparams.column_width * 2)
+# fig.subplots_adjust(hspace=0, top=0.996, right=0.996)
+fig.tight_layout()
+plt.show()
 fig.savefig("paper-plots/hist.pdf", bbox_inches="tight")

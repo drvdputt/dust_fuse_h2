52072bc458ba6c8399af0bf47d6858685e12afae
Dries Van De Putte <dvandeputte@stsci.edu>
2022-05-24 19:12:43 -0400

    Improve extinction feature plot

--- paper_extinction_sketch.py
+++ paper_extinction_sketch.py
@@ -5,27 +5,39 @@ import astropy.units as u
 
 from dust_extinction.shapes import FM90
 
+TOTAL_COLOR = 'k'
+LIN_COLOR = 'gray'
+BUMP_COLOR = 'r'
+RISE_COLOR = 'xkcd:bright blue'
+
 fig, ax = plt.subplots()
 
 # generate the curves and plot them
-x = np.arange(3.8, 8.6, 0.1) / u.micron
+# x = np.arange(3.8, 8.6, 0.1) / u.micron
+x = np.linspace(3.8, 8.6, 100) / u.micron
 
-ext_model = FM90()
-ax.plot(x, ext_model(x), label="total")
+tot = FM90()(x)
+rise = FM90(C3=0.0)(x)
+bump = FM90(C4=0.0)(x)
+lin = FM90(C3=0.0, C4=0.0)(x)
 
-ext_model = FM90(C3=0.0, C4=0.0)
-ax.plot(x, ext_model(x), label="linear term")
+ax.plot(x, bump, color=BUMP_COLOR)
+ax.plot(x, rise, color=RISE_COLOR)
+ax.plot(x, lin, color=LIN_COLOR)
+ax.plot(x, tot, label="total", color=TOTAL_COLOR, ls='dotted')
+ax.fill_between(x.value, lin, bump, color=BUMP_COLOR, alpha=0.15)
+ax.fill_between(x.value, lin, rise, color=RISE_COLOR, alpha=0.15)
 
-# ext_model = FM90(C1=0.0, C2=0.0, C4=0.0)
-ext_model = FM90(C1=0.0, C2=0.0, C4=0.0)
-ax.plot(x, ext_model(x), label="bump term")
+ax.text(4.8, 4.4, "bump\n$C_3, \\gamma, x_0$", color=BUMP_COLOR, ha='center')
+ax.text(8.30, 6.3, "rise\n$C_4$", color=RISE_COLOR, ha='center')
+ax.text(5.2, 3.0, "linear\n$C_1 + C_2x$", color=LIN_COLOR)
 
-ext_model = FM90(C1=0.0, C2=0.0, C3=0.0)
-ax.plot(x, ext_model(x), label="FUV rise term")
-
-ax.set_xlabel(r"$x$ [$\mu m^{-1}$]")
+ax.set_xlabel(r"$x$ [$\mu m^{-1}$]") # 1/lambda label
+# ax.set_xlabel(r"$\lambda [$\mu m$]") # lambda label
 ax.set_ylabel(r"$A(\lambda)/A(V)$")
 
 ax.legend(loc="best")
 fig.set_size_inches(column_width, column_width)
-fig.savefig('paper-plots/curve.pdf', bbox_inches='tight')
+# plt.show()
+# fig.savefig('paper-plots/curve.pdf', bbox_inches='tight')
+fig.savefig('paper-plots/curve.png', bbox_inches='tight', dpi=300)

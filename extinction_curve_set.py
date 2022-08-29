e175f289825c45899fa329f2c3b63975a9427c9e
Dries Van De Putte <dvandeputte@stsci.edu>
2022-08-29 14:37:27 -0400

    Additions to extinction curve set to get separate components

--- extinction_curve_set.py
+++ extinction_curve_set.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class ExtinctionCurveSet:
     """Tools to evaluate FM90 function for many stars at once"""
 
@@ -21,6 +22,26 @@ class ExtinctionCurveSet:
             self.x0_unc,
         ) = [data[p + "_unc"] for p in ExtinctionCurveSet.params_needed]
 
+    @staticmethod
+    def w_to_x(w):
+        """
+        Convert wavelength in angstrom to x value used in FM90 function
+        """
+        return 1 / (w / ExtinctionCurveSet.micron)
+
+    @staticmethod
+    def F(x):
+        """
+        The shape of the C4 term
+        """
+        return 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
+
+    def D(self, x):
+        """
+        The drude function
+        """
+        return x**2 / ((x**2 - self.x0**2) ** 2 + (x * self.gamma) ** 2)
+
     def evaluate(self, w):
         """
         Evaluate the FM90 function for all sightlines, at a given wavelength.
@@ -28,11 +49,11 @@ class ExtinctionCurveSet:
         w : wavelength in angstrom
         """
         # x is 1/w in micron
-        x = 1 / (w / ExtinctionCurveSet.micron)
+        x = self.w_to_x(w)
         # drude
-        D = x ** 2 / ((x ** 2 - self.x0 ** 2) ** 2 + (x * self.gamma) ** 2)
-        F = 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
-        Aw_AV = self.cav1 + self.cav2 * x + self.cav3 * D + self.cav4 * F
+        Aw_AV = (
+            self.cav1 + self.cav2 * x + self.cav3 * self.D(x) + self.cav4 * self.F(x)
+        )
         return Aw_AV
 
     def evaluate_unc(self, w):
@@ -40,32 +61,30 @@ class ExtinctionCurveSet:
         Uncertainty on the FM90 function, as propagated from the parameter
         uncertainties.
         """
-        x = 1 / (w / ExtinctionCurveSet.micron)
+        x = self.w_to_x(w)
 
         # drude
-        D_denom = (x ** 2 - self.x0 ** 2) ** 2 + (x * self.gamma) ** 2
-        D = x ** 2 / D_denom
-        F = 0.5392 * (x - 5.9) ** 2 + 0.05644 * (x - 5.9) ** 3
+        D_denom = (x**2 - self.x0**2) ** 2 + (x * self.gamma) ** 2
+        D = self.D(x)
+        F = self.F(x)
 
         # dD / dg = -2gx^4 / (g^2 x^2 + (x0^2 + x^2)^2)^2
         # dD / dx0 = -4mx^2(m^2+x^2)/(g^2x^2+(m^2+x^2)^2)^2
 
         # multivariate error propagation
-        D_unc_gamma = 2 * self.gamma * x ** 4 / D_denom ** 2 * self.gamma_unc
+        D_unc_gamma = 2 * self.gamma * x**4 / D_denom**2 * self.gamma_unc
         D_unc_x0 = (
-            4 * self.x0 * x ** 2 * (x ** 2 - self.x0 ** 2) / D_denom ** 2 * self.x0_unc
+            4 * self.x0 * x**2 * (x**2 - self.x0**2) / D_denom**2 * self.x0_unc
         )
-        VD_rel = (D_unc_gamma ** 2 + D_unc_x0 ** 2) / D ** 2
+        VD_rel = (D_unc_gamma**2 + D_unc_x0**2) / D**2
         # sum of relative variances
         Vcav3_rel = (self.cav3_unc / self.cav3) ** 2
         Vterm3 = (self.cav3 * D) ** 2 * (Vcav3_rel + VD_rel)
 
         VA = (
-            self.cav1_unc ** 2
+            self.cav1_unc**2
             + (self.cav2_unc * x) ** 2
             + Vterm3
             + (self.cav4_unc * F) ** 2
         )
         return np.sqrt(VA)
-
-

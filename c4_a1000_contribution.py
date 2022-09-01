9f727d1623d65c69afe95286b42c7ce85004af79
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-01 18:02:18 -0400

    Add more stuff to FUV rise contribution script

--- c4_a1000_contribution.py
+++ c4_a1000_contribution.py
@@ -14,8 +14,11 @@ from plot_fuse_results import (
     MAIN_MARKER_SIZE,
     plot_rho_box,
     format_colname,
+    plot_results_fit,
 )
 from paper_rcparams import column_width
+from math import pi
+from scipy import integrate
 
 data = get_merged_table()
 ecs = ExtinctionCurveSet(data)
@@ -34,16 +37,31 @@ a2175_av_bump = ecs.cav3 / ecs.gamma**2
 # quick check if this matters at 2175 A
 a2175_av_rise = ecs.cav4_unc * ecs.F(x)
 
+# try getting bump area (area of lorentian is pi * gamma. Not sure about drude)
+bump_area_approx = pi * ecs.gamma * ecs.cav3
+
+wgrid = np.linspace(1000, 3000, 2000)
+N = len(ecs.cav1)
+y = np.zeros((N, len(wgrid)))
+for iw, w in enumerate(wgrid):
+    y[:, iw] = ecs.cav3 * ecs.D(ecs.w_to_x(w))
+bump_area_exact = integrate.trapezoid(y, x=wgrid, axis=1)
+
 marker = itertools.cycle(("x", "+", ".", "o", "*"))
 
-def quick_rho_and_scatter(aw_av, label):
+
+def quick_rho_and_scatter(aw_av, label, quantity=None):
+    if quantity is None:
+        xparam = "nh2"
+    else:
+        xparam = quantity
     m = next(marker)
-    x = data["nh2"]
+    x = data[xparam]
     y = aw_av * data["AV"]
     rho = np.corrcoef(x, y)[0, 1]
     print(f"{label}: {rho:.2f}")
     plt.scatter(x, y, label=label + f"; r = {rho:.2f}", marker=m)
-    plt.xlabel("$N(\\rm{H_2})$")
+    plt.xlabel(xparam)
     plt.ylabel("$A(\\lambda)$")
 
 
@@ -52,13 +70,21 @@ quick_rho_and_scatter(a1000_av_rise, label="$A(V) C_4 F(x)$")
 quick_rho_and_scatter(a1000_av - a1000_av_rise, label="Rest")
 plt.legend()
 
+show_nhi = False
+if show_nhi:
+    plt.figure()
+    quick_rho_and_scatter(a1000_av_rise, label="Rise", quantity="nhtot")
+    quick_rho_and_scatter(a1000_av - a1000_av_rise, label="Rest", quantity="nhtot")
+    plt.legend()
+
 show_2175 = True
 if show_2175:
     plt.figure()
-    quick_rho_and_scatter(a2175_av, label="Total")
+    # quick_rho_and_scatter(a2175_av, label="Total")
     quick_rho_and_scatter(a2175_av_bump, label="$A(V) C_3 / \\gamma^2$")
     quick_rho_and_scatter(a2175_av - a2175_av_bump, label="Rest")
-    quick_rho_and_scatter(a2175_av_rise, label="$A(V) C_4 F(x)$")
+    quick_rho_and_scatter(bump_area_exact, label="area")
+    quick_rho_and_scatter(bump_area_approx, label="area approx")
     plt.legend()
 
 # experiment: assume that the "baseline" of the extinction cruve
@@ -76,8 +102,9 @@ if show_uvdiff:
 
 plt.legend()
 
-plt.show()
-exit()
+# plt.show()
+# exit()
+
 # copied over the main plot command from. Doing it this way because I
 # don't need any of the feature marking / ignoring that are in
 # plot_results_scatter()
@@ -96,6 +123,7 @@ def paper_style_scatter(
     s=None,
     do_rho_box=False,
     legend_label=None,
+    aw_av_diff=None,
 ):
     # get one of the x columns
     y, y_unc = get_param_and_unc(yparam, data)
@@ -124,9 +152,18 @@ def paper_style_scatter(
     ax.set_ylabel(ylabel)
     ax.set_xlabel(awlabel)
 
+    # add the difference, but with less details
+    if aw_av_diff is not None:
+        ax.scatter(
+            aw_av_diff * av, y, color="orange", alpha=0.5, label="Rest", marker="*"
+        )
+
     # assumes that x_unc is not correlated with y_unc
     if do_rho_box:
         plot_rho_box(ax, aw, y, covs, method="nocov")
+
+    # fit
+    plot_results_fit(aw, y, covs, ax)
 
     plt.gcf().set_size_inches(column_width, column_width)
 
@@ -145,17 +182,34 @@ plt.figure()
 # )
 paper_style_scatter(
     "nh2",
-    # "A(1000)",
-    "$A_{\\mathrm{rise}}(1000)$",
+    "$A(1000)$",
+    # "$A_{\\mathrm{rise}}(1000)$",
     a1000_av_rise,
     a1000_av_rise_unc,
     do_rho_box=True,
-    legend_label="FUV rise",
+    legend_label="Rise",
     s=10,
+    aw_av_diff=a1000_av - a1000_av_rise,
 )
-# plt.legend()
+plt.legend(loc="lower right")
 plt.tight_layout()
 plt.savefig("paper-plots/c4_contribution.pdf", bbox_inches="tight")
+
+# plt.figure()
+# paper_style_scatter(
+#     "nhtot",
+#     "$A(1000)$",
+#     # "$A_{\\mathrm{rise}}(1000)$",
+#     a1000_av_rise,
+#     a1000_av_rise_unc,
+#     do_rho_box=True,
+#     legend_label="Rise",
+#     s=10,
+#     aw_av_diff=a1000_av - a1000_av_rise,
+# )
+# plt.legend(loc="lower right")
+# plt.tight_layout()
+
 
 # plt.figure()
 # a1000_av, a1000_av_unc = get_param_and_unc("A1000_AV", data)

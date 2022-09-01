c81cfd2f183af349bc938b6f89e9f819788ecf6c
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-01 10:47:39 -0400

    Better bump plot in absolute feature contribution script

--- c4_a1000_contribution.py
+++ c4_a1000_contribution.py
@@ -26,37 +26,58 @@ a1000_av = ecs.evaluate(w)
 a1000_av_rise = ecs.cav4 * ecs.F(x)
 a1000_av_rise_unc = ecs.cav4_unc * ecs.F(x)
 
+
 w = 2175
 x = ecs.w_to_x(w)
 a2175_av = ecs.evaluate(w)
-a2175_av_bump = ecs.cav3 * ecs.D(x)
+a2175_av_bump = ecs.cav3 / ecs.gamma**2
+# quick check if this matters at 2175 A
+a2175_av_rise = ecs.cav4_unc * ecs.F(x)
 
 marker = itertools.cycle(("x", "+", ".", "o", "*"))
-
 
 def quick_rho_and_scatter(aw_av, label):
     m = next(marker)
     x = data["nh2"]
     y = aw_av * data["AV"]
     rho = np.corrcoef(x, y)[0, 1]
-    print(f"{label}: {rho}")
-    plt.scatter(x, y, label=label, marker=m)
+    print(f"{label}: {rho:.2f}")
+    plt.scatter(x, y, label=label + f"; r = {rho:.2f}", marker=m)
     plt.xlabel("$N(\\rm{H_2})$")
     plt.ylabel("$A(\\lambda)$")
 
 
 quick_rho_and_scatter(a1000_av, label="Total")
-quick_rho_and_scatter(a1000_av_rise, label="$C_4 F(x)$")
-# quick_rho_and_scatter(a1000_av - a1000_av_rise, label="difference")
+quick_rho_and_scatter(a1000_av_rise, label="$A(V) C_4 F(x)$")
+quick_rho_and_scatter(a1000_av - a1000_av_rise, label="Rest")
 plt.legend()
 
-show_2175 = False
+show_2175 = True
 if show_2175:
     plt.figure()
-    quick_rho_and_scatter(a2175_av, label="total")
-    quick_rho_and_scatter(a2175_av_bump, label="C_3 D(x)")
+    quick_rho_and_scatter(a2175_av, label="Total")
+    quick_rho_and_scatter(a2175_av_bump, label="$A(V) C_3 / \\gamma^2$")
+    quick_rho_and_scatter(a2175_av - a2175_av_bump, label="Rest")
+    quick_rho_and_scatter(a2175_av_rise, label="$A(V) C_4 F(x)$")
     plt.legend()
 
+# experiment: assume that the "baseline" of the extinction cruve
+# flattens at a certain wavelength. Then the FUV rise contribution is
+# the difference between A(1000) and A(w).
+show_uvdiff = False
+if show_uvdiff:
+    plt.figure()
+    for w_compare in [1250, 1500, 1750]:
+        uvdiff = a1000_av - ecs.evaluate(w_compare)
+        quick_rho_and_scatter(uvdiff, label=f"A1000 - A{w_compare}")
+# conclusion: these work amost as well as the FUV rise (about 0.88
+# for w_compare = 1250). But the shorter we go in wavelength, the
+# more this difference is dominated by C4, so this is nothing new.
+
+plt.legend()
+
+plt.show()
+exit()
 # copied over the main plot command from. Doing it this way because I
 # don't need any of the feature marking / ignoring that are in
 # plot_results_scatter()
@@ -130,7 +151,7 @@ paper_style_scatter(
     a1000_av_rise_unc,
     do_rho_box=True,
     legend_label="FUV rise",
-    s=10
+    s=10,
 )
 # plt.legend()
 plt.tight_layout()
@@ -145,5 +166,5 @@ plt.show()
 # just out of curiosity, lets also take a look at the ratio
 plt.figure()
 ratio = a1000_av_rise / a1000_av
-plt.scatter(data['nh2'], ratio)
+plt.scatter(data["nh2"], ratio)
 plt.show()

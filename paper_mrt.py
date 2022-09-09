b609caa6026d969a2e95997c0d27b09bc8546e39
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-09 16:27:36 -0400

    Fix unit and nan problem in mrt table script

--- paper_mrt.py
+++ paper_mrt.py
@@ -2,6 +2,7 @@
 
 from get_data import get_merged_table, get_fuse_h2_details
 from astropy.table import Table
+from astropy import units as u
 
 data = get_merged_table()
 
@@ -9,12 +10,21 @@ main_stars = data["Name"]
 data_comp = get_merged_table(comp=True)
 comp_stars = data_comp["Name"]
 other_stars = [
-    "HD003827, HD022586, HD177989, HD210121, HD210839, HD220057, HD239683, HD303308, HD315021"
+    "HD003827",
+    "HD022586",
+    "HD177989",
+    "HD210121",
+    "HD210839",
+    "HD220057",
+    "HD239683",
+    "HD303308",
+    "HD315021",
 ]
 
 
 def convert_gastable_to_mrt(data, fn):
     q_with_unc = ["lognhtot", "lognhi", "lognh2", "d"]
+    q_unit = [u.dex(u.cm**-2), u.dex(u.cm**-2), u.dex(u.cm**-2), u.kpc]
     q_descriptions = [
         f"Log10 of column density N({name})" for name in ["H", "HI", "H2"]
     ] + ["Distance"]
@@ -22,22 +32,23 @@ def convert_gastable_to_mrt(data, fn):
     gasmrt = Table()
     gasmrt["Name"] = data["Name"]
     gasmrt["Name"].description = "Star name"
-    for q, description in zip(q_with_unc, q_descriptions):
+    for q, description, unit in zip(q_with_unc, q_descriptions, q_unit):
         q_unc = q + "_unc"
         new_colname = q
         new_colname_unc = "e_" + new_colname
 
-        gasmrt[new_colname] = data[q]
+        gasmrt[new_colname] = data[q] * unit
         gasmrt[new_colname].description = description
-        gasmrt[new_colname_unc] = data[q_unc]
+        gasmrt[new_colname_unc] = data[q_unc] * unit
         gasmrt[new_colname_unc].description = "sigma " + description
 
     q_other = ["fh2", "denhtot", "hiref"]
     q_other_descriptions = ["Molecular fraction", "Number density", "HI data reference"]
-    for q, description in zip(q_other, q_other_descriptions):
+    q_other_unit = [None, u.cm**-3, None]
+    for q, description, unit in zip(q_other, q_other_descriptions, q_other_unit):
         new_colname = q
 
-        gasmrt[new_colname] = data[q]
+        gasmrt[new_colname] = data[q] * unit if unit is not None else data[q]
         gasmrt[new_colname].description = description
 
     gasmrt.write(fn, format="mrt", overwrite=True)
@@ -63,8 +74,12 @@ def convert_h2table_to_mrt(h2data, fn):
         new_uncname = "e_" + new_colname
 
         h2mrt[new_colname] = h2data[old_colname]
+        # do not multiply by unit here! That breaks MaskedColumn
+        # (changes masked into nan)
+        h2mrt[new_colname].unit = u.dex(u.cm**-2)
         h2mrt[new_colname].description = f"Log10 of column density H2(J = {j})"
         h2mrt[new_uncname] = h2data[old_uncname]
+        h2mrt[new_uncname].unit = u.dex(u.cm**-2)
         h2mrt[new_uncname].description = f"sigma(Log10) column density H2(J = {j})"
         h2mrt.write(fn, format="mrt", overwrite=True)
 

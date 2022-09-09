5b2ddd418e5c23251180bae97e14b04132f88519
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-09 15:22:20 -0400

    Descriptions in paper mrt

--- paper_mrt.py
+++ paper_mrt.py
@@ -6,12 +6,27 @@ from astropy.table import Table
 data = get_merged_table()
 keep_columns = ["Name"]
 q_with_unc = ["lognhtot", "lognhi", "lognh2", "d"]
-for q in q_with_unc:
-    keep_columns += [q, q + "_unc"]
+q_descriptions = [f"Log10 of column density N({name})" for name in ["H", "HI", "H2"]] + ["Distance"]
+gasmrt = Table()
+for q, description in zip(q_with_unc, q_descriptions):
+    q_unc = q + "_unc"
+    new_colname = q
+    new_colname_unc = "e_" + new_colname
+
+    gasmrt[new_colname] = data[q]
+    gasmrt[new_colname].description = description
+    gasmrt[new_colname_unc] = data[q_unc]p
+    gasmrt[new_colname_unc].description = "sigma " + description
+
 q_other = ["fh2", "denhtot", "hiref"]
-for q in q_other:
-    keep_columns += [q]
-data[keep_columns].write("paper-tables/mrt_test.dat", format="mrt", overwrite=True)
+q_other_descriptions = ["Molecular fraction", "Number density", "HI data reference"]
+for q, description in zip(q_other, q_other_descriptions):
+    new_colname = q
+
+    gasmrt[new_colname] = data[q]
+    gasmrt[new_colname].description = description
+
+gasmrt.write("paper-tables/mrt_gas.dat", format="mrt", overwrite=True)
 
 h2data = get_fuse_h2_details(components=True, stars=data["Name"])
 h2mrt = Table()
@@ -29,4 +44,4 @@ for j in range(8):
     h2mrt[new_uncname].description = f"sigma(Log10) column density H2(J = {j})"
 
 
-h2data[keep_columns_h2].write("paper-tables/mrt_h2.dat", format="mrt", overwrite=True)
+h2mrt.write("paper-tables/mrt_h2.dat", format="mrt", overwrite=True)

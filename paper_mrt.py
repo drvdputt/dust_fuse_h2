5c5d0c1f7024af42b3581ddf89b59526f2e4011a
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-09 15:51:29 -0400

    Separate tables for main and comp stars

--- paper_mrt.py
+++ paper_mrt.py
@@ -4,44 +4,66 @@ from get_data import get_merged_table, get_fuse_h2_details
 from astropy.table import Table
 
 data = get_merged_table()
-keep_columns = ["Name"]
-q_with_unc = ["lognhtot", "lognhi", "lognh2", "d"]
-q_descriptions = [f"Log10 of column density N({name})" for name in ["H", "HI", "H2"]] + ["Distance"]
-gasmrt = Table()
-for q, description in zip(q_with_unc, q_descriptions):
-    q_unc = q + "_unc"
-    new_colname = q
-    new_colname_unc = "e_" + new_colname
 
-    gasmrt[new_colname] = data[q]
-    gasmrt[new_colname].description = description
-    gasmrt[new_colname_unc] = data[q_unc]p
-    gasmrt[new_colname_unc].description = "sigma " + description
-
-q_other = ["fh2", "denhtot", "hiref"]
-q_other_descriptions = ["Molecular fraction", "Number density", "HI data reference"]
-for q, description in zip(q_other, q_other_descriptions):
-    new_colname = q
-
-    gasmrt[new_colname] = data[q]
-    gasmrt[new_colname].description = description
-
-gasmrt.write("paper-tables/mrt_gas.dat", format="mrt", overwrite=True)
-
-h2data = get_fuse_h2_details(components=True, stars=data["Name"])
-h2mrt = Table()
-h2mrt["Name"] = h2data["Name"]
-for j in range(8):
-    # the densities from J = 0 to J = 7
-    old_colname = f"lognj{j}"
-    old_uncname = old_colname + "_unc"
-    new_colname = f"N{j}"
-    new_uncname = "e_" + new_colname
-
-    h2mrt[new_colname] = h2data[old_colname]
-    h2mrt[new_colname].description = f"Log10 of column density H2(J = {j})"
-    h2mrt[new_uncname] = h2data[old_uncname]
-    h2mrt[new_uncname].description = f"sigma(Log10) column density H2(J = {j})"
+main_stars = data["Name"]
+data_comp = get_merged_table(comp=True)
+comp_stars = data_comp["Name"]
 
 
-h2mrt.write("paper-tables/mrt_h2.dat", format="mrt", overwrite=True)
+def convert_gastable_to_mrt(data, fn):
+    q_with_unc = ["lognhtot", "lognhi", "lognh2", "d"]
+    q_descriptions = [
+        f"Log10 of column density N({name})" for name in ["H", "HI", "H2"]
+    ] + ["Distance"]
+
+    gasmrt = Table()
+    gasmrt["Name"] = data["Name"]
+    gasmrt["Name"].description = "Star name"
+    for q, description in zip(q_with_unc, q_descriptions):
+        q_unc = q + "_unc"
+        new_colname = q
+        new_colname_unc = "e_" + new_colname
+
+        gasmrt[new_colname] = data[q]
+        gasmrt[new_colname].description = description
+        gasmrt[new_colname_unc] = data[q_unc]
+        gasmrt[new_colname_unc].description = "sigma " + description
+
+    q_other = ["fh2", "denhtot", "hiref"]
+    q_other_descriptions = ["Molecular fraction", "Number density", "HI data reference"]
+    for q, description in zip(q_other, q_other_descriptions):
+        new_colname = q
+
+        gasmrt[new_colname] = data[q]
+        gasmrt[new_colname].description = description
+
+    gasmrt.write(fn, format="mrt", overwrite=True)
+
+
+convert_gastable_to_mrt(data, "paper-tables/mrt_gas.dat")
+convert_gastable_to_mrt(data_comp, "paper-tables/mrt_gas_comp.dat")
+
+h2data = get_fuse_h2_details(components=True, stars=main_stars)
+h2data_comp = get_fuse_h2_details(components=True, stars=comp_stars)
+
+
+def convert_h2table_to_mrt(h2data, fn):
+    h2mrt = Table()
+    h2mrt["Name"] = h2data["Name"]
+    h2mrt["Name"].description = "Star name"
+    for j in range(8):
+        # the densities from J = 0 to J = 7
+        old_colname = f"lognj{j}"
+        old_uncname = old_colname + "_unc"
+        new_colname = f"N{j}"
+        new_uncname = "e_" + new_colname
+
+        h2mrt[new_colname] = h2data[old_colname]
+        h2mrt[new_colname].description = f"Log10 of column density H2(J = {j})"
+        h2mrt[new_uncname] = h2data[old_uncname]
+        h2mrt[new_uncname].description = f"sigma(Log10) column density H2(J = {j})"
+        h2mrt.write(fn, format="mrt", overwrite=True)
+
+
+h2mrt = convert_h2table_to_mrt(h2data, "paper-tables/mrt_h2.dat")
+h2mrt_comp = convert_h2table_to_mrt(h2data_comp, "paper-tables/mrt_h2_comp.dat")
